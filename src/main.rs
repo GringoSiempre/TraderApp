f6eed442b2fd1647651590cf4981a7a4838c9802
Gringo Siempre <ceo@moonward.cz>
2024-10-23 22:36:29 +0200

    Using egui UI. Added button which display current time when clicked.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use eframe::egui;
+use chrono::Local;
+
+struct MyApp {
+    show_time: bool,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            show_time: false,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Центрируем содержимое окна.
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                // Настройка размера кнопки.
+                let size = egui::Vec2::splat(60.0); // Размер кнопки.
+                // Создаем кнопку.
+                if ui.add_sized(size, egui::Button::new("Time")).clicked() {
+                    self.show_time = true;
+                }
+                // Отображаем текущее время после нажатия на кнопку.
+                if self.show_time {
+                    let now = Local::now();
+                    ui.label(now.format("%H:%M:%S").to_string());
+                }
+            });
+        });
+        ctx.request_repaint();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Окно с кнопкой",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    ).expect("TODO: panic message");
 }
